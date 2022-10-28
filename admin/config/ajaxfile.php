e288e6762f6ba7fe8ae4c4ffdfc12516a5a8b4e9
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2022-10-28 16:08:10 -0300

    Revert "Revert "mejorando modal e instalando sweet alert""
    This reverts commit d893170f86a25859ccb1ec19903488284688bafa.

--- admin/config/ajaxfile.php
+++ admin/config/ajaxfile.php
@@ -12,27 +12,65 @@ $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($listaProductos as $producto) {
 ?>
-<table style="border:0;" width='100%'>
-<tr>
-    <td width="300"><img src="images/<?php echo $producto['Prod_Imagen']; ?>">
-    <td style="padding:20px;">
-    <p>Nombre <?php echo $producto['Prod_Nombre']; ?></p>
-    <p>Descripción<?php echo $producto['Prod_Descripcion']; ?></p>
-    <p>Precio:<?php echo $producto['Prod_Precio']; ?></p>
-    </td>
-</tr>
-</table>
-<br>
-<form class="modal-footer" method="post">
-    <input type="hidden" name="Local_Nombre" id="Local_Nombre" value="<?php echo openssl_encrypt($localNomb,cod,key); ?>">
-    <input type="hidden" name="Prod_Id" id="Prod_Id" value="<?php echo openssl_encrypt($producto['Prod_Id'],cod,key); ?>">
-    <input type="hidden" name="Prod_Nombre" id="Prod_Nombre" value="<?php echo openssl_encrypt($producto['Prod_Nombre'],cod,key); ?>">
-    <input type="hidden" name="Prod_Imagen" id="Prod_Imagen" value="<?php echo openssl_encrypt($producto['Prod_Imagen'],cod,key); ?>">
-    <input type="hidden" name="Prod_Precio" id="Prod_Precio" value="<?php echo openssl_encrypt($producto['Prod_Precio'],cod,key); ?>">
-    <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,cod,key); ?>">
-    <input type="number" class="form-control-sm conta" name="conta" id="conta" placeholder="Cantidad de compra">
-    <button class="botonAgregar btn btn-warning" name="btnAccion" value="Agregar" type="submit">
-        Agregar a carrito 
-    </button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="padreTR">
+
+        <div class="flexTR">
+
+            <table class="tableTR">
+                <tr class="containerTR">
+                    <td class="contenedorImagen"><img class="imageProducto"
+                            src="https://via.placeholder.com/468x60?text=Visit+Blogging.com+NowC/O https://placeholder.com/">
+                    <td class="descripcionProductos">
+                        <p>Nombre: <?php echo $producto['Prod_Nombre']; ?></p>
+                        <p>Descripción: <?php echo $producto['Prod_Descripcion']; ?></p>
+                        <p>Precio: $<?php echo $producto['Prod_Precio']; ?></p>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+
+    </div>
+    <br>
+    <form class="modal-footer" method="post">
+        <input type="hidden" name="Local_Nombre" id="Local_Nombre"
+            value="<?php echo openssl_encrypt($localNomb,cod,key); ?>">
+        <input type="hidden" name="Prod_Id" id="Prod_Id"
+            value="<?php echo openssl_encrypt($producto['Prod_Id'],cod,key); ?>">
+        <input type="hidden" name="Prod_Nombre" id="Prod_Nombre"
+            value="<?php echo openssl_encrypt($producto['Prod_Nombre'],cod,key); ?>">
+        <input type="hidden" name="Prod_Imagen" id="Prod_Imagen"
+            value="<?php echo openssl_encrypt($producto['Prod_Imagen'],cod,key); ?>">
+        <input type="hidden" name="Prod_Precio" id="Prod_Precio"
+            value="<?php echo openssl_encrypt($producto['Prod_Precio'],cod,key); ?>">
+        <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,cod,key); ?>">
+        <div class="flexBotones">
+
+            <input type="number" class="form-control-sm conta" name="conta" id="conta" placeholder="Unidades">
+            <button class="botonAgregar btn btn-warning" name="btnAccion" value="Agregar" type="submit" id="agregarr">
+                Agregar a carrito
+            </button>
+
+        </div>
+    </form>
+
+</body>
+
+
+
+<script src="../../Cata-Food/js/toast.js"></script>
+
+</html>
 <?php } ?>
