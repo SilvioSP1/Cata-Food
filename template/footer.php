bb95e03a6b0cc6f801e87d463efd1b3f61df5307
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-01 19:43:33 -0300

    modificando footer

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,4 @@
-
-      </section>
+</section>
 
 <!-- CONTEINER -->
 
@@ -83,7 +82,7 @@
 <script src="../../Cata-Food/js/rate.js?v=<?php echo time(); ?>"></script>
 <script src="../../Cata-Food/js/filtradoComidas.js?v=<?php echo time(); ?>"></script>
 <script src="../../Cata-Food/js/darkMode.js?v=<?php echo time(); ?>"></script>
-<script type="module" src="../../Cata-Food/js/mail.js?v=<?php echo time(); ?>"></script>
+
 
 
 <!-- JavaScript Bundle with Popper -->
@@ -101,8 +100,8 @@ crossorigin="anonymous"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="../../Cata-food/js/chat.js?v=<?php echo time();?>"></script>
-<script src="../Cata-food/js/response.js?v<?php echo time();?>"></script>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
