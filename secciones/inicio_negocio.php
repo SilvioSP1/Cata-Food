2ad261fff84251c10e80dc91f764fc28d91c10f8
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2023-02-02 17:37:10 -0300

    cambio

--- secciones/inicio_negocio.php
+++ secciones/inicio_negocio.php
@@ -72,39 +72,6 @@ error_reporting(0);
     }
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cata Food</title>
-  <!-- CSS only Bootstrap-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
-  <!-- CSS -->
-  <link rel="stylesheet" href="../css/estilos.css">
-
-  <!-- animation css -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-
-  <link rel="icon" href="../img/index/logo_redondo2.png">
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-
-</head>
-
-
-<body class="boiler">
-
-  <main>
-
   <div class="flexDark">
 
     <div class="colorMode__dark">
