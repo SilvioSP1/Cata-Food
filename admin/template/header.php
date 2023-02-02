0aaa9f731d396f6115742fb1fc0917fbbcc42806
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2023-02-02 18:22:37 -0300

    Admin Cambios 2

--- admin/template/header.php
+++ admin/template/header.php
@@ -26,6 +26,8 @@ if ($_SESSION['usuario']!=="Sin Loguearse") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+    <link rel="icon" href="../../img/index/logo_redondo2.png">
     
   </head>
   <body>
@@ -44,13 +46,13 @@ if ($_SESSION['usuario']!=="Sin Loguearse") {
                         <a class="nav-link active" href="admin/index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="<?php echo $url;?>/admin/secciones/locales.php">Locales</a>
+                      <a class="nav-link" href="admin/secciones/locales.php">Locales</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="<?php echo $url;?>/admin/secciones/productos.php">Productos</a>
+                      <a class="nav-link" href="admin/secciones/productos.php">Productos</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="<?php echo $url;?>/admin/secciones/usuarios.php">Usuarios</a>
+                      <a class="nav-link" href="admin/secciones/usuarios.php">Usuarios</a>
                     </li>
                     <li class="nav-item dropdown">
                         <!-- <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
