221ec9556aa39d9ba9bef37b6c370660d9e1123f
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2023-02-07 19:35:26 -0300

    Update formulario_pago.php

--- secciones/formulario_pago.php
+++ secciones/formulario_pago.php
@@ -280,7 +280,7 @@ $_SESSION['condicion'] = 1;
                             }
                             return todo_correcto;
                             }
-                            nombre.addEventListener("keyup", validar);
+                            nombre.addEventListener("click", validar);
                         </script>
 
 
