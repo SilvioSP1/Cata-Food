9d3de6df4d55097228ae1280834915bf005b53ca
SilvioSP1 <104458705+SilvioSP1@users.noreply.github.com>
2023-02-08 17:54:18 -0300

    Update formulario_pago.php

--- secciones/formulario_pago.php
+++ secciones/formulario_pago.php
@@ -202,7 +202,7 @@ $_SESSION['condicion'] = 1;
                                                 <input type="radio" value="check2" name="input">
                                                 <label for="check2">Sin envio</label> -->
 
-                                                <input type="radio" name="lang" value="hide" id="hide" onclick="showHideDiv(1)">
+                                                <input type="radio" name="lang" value="hide" id="hide" onclick="showHideDiv(1)" checked>
                                                 Con envio
                                                 <input type="radio" name="lang" value="show" id="show" onclick="showHideDiv(2)">
                                                 Sin envio
