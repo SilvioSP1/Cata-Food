2fba857100d2885530aef892207dd07ffd902fc8
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-09 03:01:16 -0300

    probanding

--- js/radioInputs.js
+++ js/radioInputs.js
@@ -23,7 +23,7 @@ function validar() {
     devolverá false.*/
 
 
-    if (document.querySelector('input[name="sin"]:checked').value == 'hide') {
+    if (document.querySelector('input[name="sin"]:checked').checked==false) {
         if (document.getElementById('nombre').value.length < 2) {
             todo_correcto = false;
         } else {
