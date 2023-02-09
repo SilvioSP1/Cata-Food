614b144e976a4022b888f188f18ed347e53d71c0
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-09 01:44:05 -0300

    creando toast propio

--- secciones/formulario_pago.php
+++ secciones/formulario_pago.php
@@ -158,6 +158,27 @@ $_SESSION['condicion'] = 1;
 
                                 </div> -->
 
+                                <div class="toast">
+
+                                    <div class="toast-content">
+
+                                        <i class="fas fa-solid fa-check check"></i>
+
+                                        <div class="message">
+
+                                            <span class="text text-1">Success</span>
+                                            <span class="text text-2">Your changes has been saved</span>
+
+                                        </div>
+
+                                    </div>
+
+                                    <i class="fa-solid fa-xmark close"></i>
+
+                                    <div class="progress"></div>
+
+                                </div>
+
 
 
 
