b65ad7306a5e7346444a797539983a955cb0ff0b
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-09 02:40:06 -0300

    funcionara?

--- js/radioInputs.js
+++ js/radioInputs.js
@@ -86,3 +86,4 @@ altura.addEventListener("keyup", validar);
 telefono.addEventListener("keyup", validar);
 piso.addEventListener("keyup", validar);
 radio.addEventListener("click", validar);
+radio2.addEventListener("click", validar)
