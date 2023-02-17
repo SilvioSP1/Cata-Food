5218a19de36bf6fa2a0de0902df6ad65bf61ec50
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-17 00:52:30 -0300

    probamos

--- secciones/local.php
+++ secciones/local.php
@@ -325,7 +325,7 @@ $localAbierto = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
   <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header modalDark">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Local</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
