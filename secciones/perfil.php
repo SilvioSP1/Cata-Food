b15613ab253a4bacf1aa187107a633f96082bbb5
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-17 01:06:07 -0300

    cambiamos modal header

--- secciones/perfil.php
+++ secciones/perfil.php
@@ -244,7 +244,7 @@ switch ($accion) {
     <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header modalPerfil">
           <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Perfil</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
