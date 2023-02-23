1dfabda281855d14ca45109af38c0981e3047916
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-23 17:00:30 -0300

    sacamos decorado

--- secciones/local.php
+++ secciones/local.php
@@ -308,7 +308,7 @@ $localAbierto = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
                         </button>
                     </li>
                   </ol>
-                  <button class="buttonReporte"><a href="../reporte/reporte.php">Reporte</a></button>
+                  <button class="buttonReporte"><a href="../reporte/reporte.php" class="enlaceReporte">Reporte</a></button>
                 </div>
                 <!-- <div class="progress rounded" style="height: 5px;">
                   <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
