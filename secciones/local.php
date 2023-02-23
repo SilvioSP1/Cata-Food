21b8eb9e9ab7d78e7fbb4858ede26b6dc1993f04
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-23 17:03:01 -0300

    cambiamos el icono

--- secciones/local.php
+++ secciones/local.php
@@ -308,7 +308,7 @@ $localAbierto = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
                         </button>
                     </li>
                   </ol>
-                  <button class="buttonReporte"><a href="../reporte/reporte.php" class="enlaceReporte">Reporte 🡻</a></button>
+                  <a href="../reporte/reporte.php" class="enlaceReporte"><button class="buttonReporte">Reporte 🡳</button></a>
                 </div>
                 <!-- <div class="progress rounded" style="height: 5px;">
                   <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
