0b86307ca8f8dc8e35fb5cc23854013d7076b21e
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-24 22:20:21 -0300

    mejoramos respuesta

--- js/response.js
+++ js/response.js
@@ -19,7 +19,7 @@ function getBotResponse(input) {
     
     }else if(input == "El sitio no carga correctamente" || input == "el sitio no carga correctamente" || input == "5"){
 
-        return "En ese caso te recomendamos que actualices la pagina , que cambies de navegador o que elimines las cookies del sitio.";
+        return "En ese caso te recomendamos: <br> Actualizar la página <br> Cambiar de navegador <br> Eliminar cookies del sitio web";
     
     }
     else {
