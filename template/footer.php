8955f17ddc6bed04a80ff54be6be0e309ed1d56f
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-06-26 04:07:59 -0300

    Modificando enlaces footer

--- template/footer.php
+++ template/footer.php
@@ -36,7 +36,7 @@
 
         <div class="divFooter">
           <a download="Politicas" href="../doc/Políticas Cata Food.docx" class="politicasSitio">Politicas Cata Food</a>
-          <a href="https://autogestion.produccion.gob.ar/consumidores" target="_blank" style="color: white">Defensa al consumidor</a>
+          <a href="https://autogestion.produccion.gob.ar/consumidores" style="color: white"  target='blank'>Defensa al consumidor</a>
         </div> 
 
       </section>
